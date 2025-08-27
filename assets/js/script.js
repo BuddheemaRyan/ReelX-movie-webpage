5082d4303d397cb6134a7caac73620eccfa7ab9e
Buddheema Ryan <buddimaraya16@gmail.com>
2025-08-27 12:53:35 +0530

    Demo Movie Details

--- assets/js/script.js
+++ assets/js/script.js
@@ -277,4 +277,52 @@ class MovieApp{
         };
     }
 
+    getDemoMovieDetails(imdbId){
+        const demoDetails={
+            "tt0468569":{
+               Title: "The Dark Knight",
+                Year: "2008",
+                Rated: "PG-13",
+                Released: "18 Jul 2008",
+                Runtime: "152 min",
+                Genre: "Action, Crime, Drama",
+                Director: "Christopher Nolan",
+                Writer: "Jonathan Nolan, Christopher Nolan, David S. Goyer",
+                Actors: "Christian Bale, Heath Ledger, Aaron Eckhart",
+                Plot: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+                Language: "English, Mandarin",
+                Country: "United States, United Kingdom",
+                Awards: "Won 2 Oscars. 159 wins & 163 nominations total",
+                Poster: "https://images.pexels.com/photos/7991579/pexels-photo-7991579.jpeg?auto=compress&cs=tinysrgb&w=300&h=450&fit=crop",
+                Ratings: [
+                    { Source: "Internet Movie Database", Value: "9.0/10" },
+                    { Source: "Rotten Tomatoes", Value: "94%" },
+                    { Source: "Metacritic", Value: "84/100" }
+                ],
+                Metascore: "84",
+                imdbRating: "9.0",
+                imdbVotes: "2,558,416",
+                imdbID: "tt0468569",
+                Type: "movie",
+                DVD: "09 Dec 2008",
+                BoxOffice: "$534,858,444",
+                Production: "Warner Bros. Pictures",
+                Website: "N/A",
+                Response: "True"
+            }
+        };
+        return demoDetails[imdbId] || {
+             Title: "Demo Movie",
+            Year: "2023",
+            Genre: "Demo",
+            Director: "Demo Director",
+            Actors: "Demo Actor 1, Demo Actor 2",
+            Plot: "This is a demo movie for testing purposes. Replace the API key to get real movie data.",
+            Poster: "https://images.pexels.com/photos/7991579/pexels-photo-7991579.jpeg?auto=compress&cs=tinysrgb&w=300&h=450&fit=crop",
+            imdbRating: "8.5",
+            Response: "True"
+        };
+        
+    }
+
 }
